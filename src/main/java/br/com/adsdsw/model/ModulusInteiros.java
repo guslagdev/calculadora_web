46dd0bbef9a80c52452760090295ec621323bbcf
Gustavo Lagares <152724645+guslagdev@users.noreply.github.com>
2023-12-02 16:23:53 -0300

    Update ModulusInteiros.java

--- src/main/java/br/com/adsdsw/model/ModulusInteiros.java
+++ src/main/java/br/com/adsdsw/model/ModulusInteiros.java
@@ -1,5 +1,14 @@
 package br.com.adsdsw.model;
 
+/**
+* Esta classe contempla a lógica e tratamento 
+* para calculos de multiplicação de inteiros.
+*
+* @author  Colab001
+* @version 1.0
+* @since   05/08/2023
+*/
+
 public class ModulusInteiros {
 	private int n1;
 	private int n2;
